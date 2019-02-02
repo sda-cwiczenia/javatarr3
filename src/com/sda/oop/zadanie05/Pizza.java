5c0414f1b3486799da7ee5f86fd7c4d2d2e2d05c
Piatkowski <irek.piatkowski@nokia.com>
2019-02-02 23:07:43 +0100

    pierwsza działająca wersja ćwiczenia

---
+++ src/com/sda/oop/zadanie05/Pizza.java
@@ -0,0 +1,4 @@
+package com.sda.oop.zadanie05;
+
+public interface Pizza {
+}
---
+++ src/com/sda/oop/zadanie05/doughs/AllAmericanThin.java
@@ -0,0 +1,8 @@
+package com.sda.oop.zadanie05;
+
+public class AllAmericanThin implements PizzaDough {
+    @Override
+    public void preparePizzaDough() {
+        System.out.println("Przygotowywanie ciasta AllAmericanThin");
+    }
+}
